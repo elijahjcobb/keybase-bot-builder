1d54ecc02ca697eef7c6a5b080b893b2bcb1ad1b
Elijah Cobb <elijah@elijahcobb.com>
2020-02-05 11:54:12 -0500

    setup some types

--- ts/index.ts
+++ ts/index.ts
@@ -5,4 +5,52 @@
  * github.com/elijahjcobb
  */
 
-console.log("Hello, world!");
+import Keybase from "keybase-bot";
+import {MsgSummary} from "keybase-bot/lib/types/chat1";
+
+type Types = "string" | "int" | "double" | "boolean" | "char";
+
+interface Message {
+	keybaseMessage: MsgSummary;
+	content: string;
+	senderId: string;
+	channel: string;
+	deviceName: string;
+	username: string;
+	time: number;
+}
+
+interface Command {
+	name: string;
+	parameters: {
+		key: string;
+		type: Types
+	}[];
+	handler: (message: Message) => Promise<string>;
+}
+
+interface CommandModule {
+	name: string;
+	commands: Command[];
+}
+
+(async (): Promise<void> => {
+
+	const bot: Keybase = new Keybase();
+
+	console.log("Will Init User");
+	await bot.init("otto_bot", "material paddle wave echo giant able machine control first tape say meat wet");
+	console.log("Did Init User");
+
+	console.log("Now Listening");
+	await bot.chat.watchAllChannelsForNewMessages(async(message: MsgSummary): Promise<void> => {
+		message.
+		console.log(`${message.sender.username}@${message.sender.deviceName} '${message.content.text?.body}'`);
+		await bot.chat.send(message.channel, {body: `No you are a ${message.content.text?.body}!`});
+		await bot.chat.command
+
+	});
+
+	await bot.deinit();
+
+})().then((): void => {}).catch((err: any): void => console.error(err));
